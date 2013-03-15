09f26d15c218081b9491cd022d16c1def6c43c4d
Kristian Rosenvold <krose@nets.eu>
2013-03-15 08:51:34 +0100

    Made the stdout/stderr capture properly differentiate between stdout/stderr

--- src/main/java/eu/nets/utils/jetty/embedded/StdoutRedirect.java
+++ src/main/java/eu/nets/utils/jetty/embedded/StdoutRedirect.java
@@ -1,7 +1,9 @@
 package eu.nets.utils.jetty.embedded;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.OutputStream;
 import java.io.PrintStream;
 
 import static com.google.common.base.CharMatcher.anyOf;
@@ -9,26 +11,34 @@ import static com.google.common.base.Splitter.on;
 
 
 /**
- * Shameless ripoff from stackoverflow.com
+ * Not-entirely Shameless ripoff from stackoverflow.com
  */
 public class StdoutRedirect {
 
     public static void tieSystemOutAndErrToLog() {
 
-        // Be sure log4j has been initialized here.
+        // Be sure log4j has been initialized here, which is always the case for embedded-jetty.
         //  this shameless ripoff causes an infinite loop ehn log4j has not been properly initialized...
         //log4j -> logs to standard-err when it finds no appenders, standard err has been redirected using slf 4j
         //and here we go...
-        System.setOut(createLoggingProxy(System.out));
-        System.setErr(createLoggingProxy(System.err));
+        System.setOut(createLoggingProxy(System.out, "stdout", true));
+        System.setErr(createLoggingProxy(System.err, "stderr", false));
     }
 
-    private static PrintStream createLoggingProxy(final PrintStream realPrintStream) {
+
+    private static PrintStream createLoggingProxy(final PrintStream realPrintStream, final String stream, final boolean stdout) {
         return new PrintStream(realPrintStream) {
+
+            private Logger logger = LoggerFactory.getLogger(stdout ? "stdout" : "stderr");
+
+
             public void print(final String string) {
                 Iterable<String> lines = on(anyOf("\r\n")).split(string);
                 for (String line : lines) {
-                    LoggerFactory.getLogger("stdout").info(line);
+                    if (stdout)
+                        logger.info(line);
+                    else
+                        logger.warn(line);
                 }
             }
 
