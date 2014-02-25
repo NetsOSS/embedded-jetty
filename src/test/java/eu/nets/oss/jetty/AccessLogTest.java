4a9e291e2606c8bdb18268d80643600dd74e8fca
Ivar Refsdal <ivref@nets.eu>
2014-02-25 11:44:06 +0100

    o Add requestLogger option to createRootServletContextHandler

---
+++ src/test/java/eu/nets/oss/jetty/AccessLogTest.java
@@ -0,0 +1,145 @@
+package eu.nets.oss.jetty;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.RequestLog;
+import org.eclipse.jetty.server.Response;
+import org.eclipse.jetty.server.Server;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.fail;
+
+public class AccessLogTest {
+
+    private static final String contextPath = "/abc";
+    private static final int port = 8199;
+    private static Server jetty;
+    static final AtomicInteger lastStatus = new AtomicInteger(0);
+
+    @BeforeClass
+    public static void startJetty() {
+        EmbeddedJettyBuilder builder = getBuilder();
+        EmbeddedJettyBuilder.ServletContextHandlerBuilder handlerBuilder = builder.createRootServletContextHandler("", new RequestLogEmpty() {
+            @Override
+            public void log(Request request, Response response) {
+                lastStatus.set(response.getStatus());
+            }
+        });
+
+        handlerBuilder.addServlet(new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                if (req.getRequestURI().contains("/found")) {
+                    resp.setStatus(200);
+                } else {
+                    resp.setStatus(404);
+                }
+            }
+        }).mountAtPath("/test/*");
+        jetty = builder.buildJetty();
+        try {
+            jetty.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterClass
+    public static void stopJetty() {
+        try {
+            jetty.stop();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testAccessLog() throws Exception {
+        URL url = new URL(String.format("http://localhost:%d%s/test/found/abc", port, contextPath));
+        try (InputStream is = url.openStream()) {
+        }
+        assertEquals(200, lastStatus.get());
+
+        url = new URL(String.format("http://localhost:%d%s/test/notfound/abc", port, contextPath));
+        try (InputStream is = url.openStream()) {
+            fail("Expected 404");
+        } catch (FileNotFoundException exception) {
+            assertEquals(404, lastStatus.get());
+        }
+    }
+
+    private static EmbeddedJettyBuilder getBuilder() {
+        ContextPathConfig config = new StaticConfig(contextPath, port);
+        return new EmbeddedJettyBuilder(config, true);
+    }
+
+    private static class RequestLogEmpty implements RequestLog {
+        @Override
+        public void log(Request request, Response response) {
+
+        }
+
+        @Override
+        public void start() throws Exception {
+
+        }
+
+        @Override
+        public void stop() throws Exception {
+
+        }
+
+        @Override
+        public boolean isRunning() {
+            return false;
+        }
+
+        @Override
+        public boolean isStarted() {
+            return false;
+        }
+
+        @Override
+        public boolean isStarting() {
+            return false;
+        }
+
+        @Override
+        public boolean isStopping() {
+            return false;
+        }
+
+        @Override
+        public boolean isStopped() {
+            return false;
+        }
+
+        @Override
+        public boolean isFailed() {
+            return false;
+        }
+
+        @Override
+        public void addLifeCycleListener(Listener listener) {
+
+        }
+
+        @Override
+        public void removeLifeCycleListener(Listener listener) {
+
+        }
+    }
+
+}
