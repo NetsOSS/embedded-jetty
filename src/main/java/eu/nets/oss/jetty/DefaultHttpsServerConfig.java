27b4fb1687e53fea6ad27d46fff07c3d0d34b151
Leif John Korshavn <ljkor@nets.eu>
2015-03-09 15:34:24 +0100

    Disabling weak ciphers

--- src/main/java/eu/nets/oss/jetty/DefaultHttpsServerConfig.java
+++ src/main/java/eu/nets/oss/jetty/DefaultHttpsServerConfig.java
@@ -15,15 +15,20 @@ public class DefaultHttpsServerConfig implements HttpsServerConfig {
         String httpsServerKeyStorePathProperty = System.getProperty("https.server.key.store.path");
         String httpsServerKeyStorePasswordProperty = System.getProperty("https.server.key.store.password");
 
-        if (httpsServerPortProperty == null) { throw new IllegalArgumentException("Must have HTTPS Server port defined as system property 'https.server.port'.");}
-        if (httpsServerKeyStorePathProperty == null) { throw new IllegalArgumentException("Must have key store resource/file defined as system property 'https.server.key.store.path'."); }
-        if (httpsServerKeyStorePasswordProperty == null) { throw new IllegalArgumentException("Must have key store password defined as system property 'https.server.key.store.password'.");}
+        if (httpsServerPortProperty == null) {
+            throw new IllegalArgumentException("Must have HTTPS Server port defined as system property 'https.server.port'.");
+        }
+        if (httpsServerKeyStorePathProperty == null) {
+            throw new IllegalArgumentException("Must have key store resource/file defined as system property 'https.server.key.store.path'.");
+        }
+        if (httpsServerKeyStorePasswordProperty == null) {
+            throw new IllegalArgumentException("Must have key store password defined as system property 'https.server.key.store.password'.");
+        }
 
         httpsServerPort = Integer.valueOf(httpsServerPortProperty);
         httpsServerKeyStoreResource = Resource.newClassPathResource(httpsServerKeyStorePathProperty);
         httpsServerKeyStorePassword = httpsServerKeyStorePasswordProperty;
     }
-
 
 
     @Override
@@ -37,14 +42,22 @@ public class DefaultHttpsServerConfig implements HttpsServerConfig {
         sslContextFactory.setKeyStoreResource(httpsServerKeyStoreResource);
         sslContextFactory.setKeyStorePassword(httpsServerKeyStorePassword);
         sslContextFactory.setProtocol("TLSv1.2");
-        
+
         sslContextFactory.setExcludeCipherSuites( // Explicitly disable weak ciphers.
                 "SSL_RSA_WITH_DES_CBC_SHA",
                 "SSL_DHE_RSA_WITH_DES_CBC_SHA",
                 "SSL_DHE_DSS_WITH_DES_CBC_SHA",
+                "SSL_RSA_EXPORT_WITH_RC4_40_MD5",
                 "SSL_RSA_EXPORT_WITH_DES40_CBC_SHA",
                 "SSL_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA",
-                "SSL_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA"
+                "SSL_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA",
+                "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA",
+                "TLS_ECDHE_RSA_WITH_RC4_128_SHA",
+                "SSL_RSA_WITH_RC4_128_SHA",
+                "TLS_ECDH_ECDSA_WITH_RC4_128_SHA",
+                "TLS_ECDH_RSA_WITH_RC4_128_SHA",
+                "SSL_RSA_WITH_RC4_128_MD5"
+                
         );
 
         return sslContextFactory;
